396f960e9bf51f9fa492fcb1c9186bde6b4289ef
Hooman Baradaran <hooman@hoomanb.com>
2020-05-31 11:49:58 -0400

    Better parsing + refresh
    - Moved feed list to it's own file
    - Changed feed to RSS (Using CBC feed as demo)
    - Remove html from description
    - Style changes
    - Implement refresh button

---
+++ lib/feed_list.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
+import 'package:webfeed/webfeed.dart';
+import 'package:html_unescape/html_unescape.dart';
+
+class FeedList extends StatefulWidget {
+  FeedList({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _FeedListState createState() => new _FeedListState();
+}
+
+class _FeedListState extends State<FeedList> {
+
+final String feedUrl = 'http://rss.cbc.ca/lineup/topstories.xml';
+
+  Future<RssFeed> _feed;
+
+  @override
+  void initState() {
+    _feed = _fetchFeed();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var unescape = new HtmlUnescape();
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Flexible(
+              child: FutureBuilder<RssFeed>(
+                future: _feed,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data.items.length,
+                      itemBuilder: (BuildContext context, int position) {
+                        // Remove html from description
+                        var description = snapshot.data.items[position].description != null ? parse(snapshot.data.items[position].description).documentElement.text : '...';
+
+                        return Card(
+                          child: ListTile(
+                            title: Text(
+                              unescape.convert(snapshot.data.items[position].title),
+                              style: Theme.of(context).textTheme.headline5
+                            ),
+                            subtitle: Padding(
+                              child: Text(
+                                description.trim(),
+                                style: Theme.of(context).textTheme.bodyText2
+                              ),
+                              padding: EdgeInsets.only(top: 10)
+                            ),
+                            contentPadding: EdgeInsetsDirectional.only(
+                              start: 10.0,
+                              end: 10.0,
+                              top: 15.0,
+                              bottom: 15.0,
+                            ),
+                          )
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("ERROR: ${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
+                }
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _refresh,
+        tooltip: 'Refresh',
+        child: new Icon(Icons.refresh),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<RssFeed> _fetchFeed() async {
+    final response = await http.get(feedUrl);
+
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      return RssFeed.parse(response.body);
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+
+  void _refresh() async {
+    setState(() {
+      _feed = _fetchFeed();
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:webfeed/webfeed.dart';
-import 'package:html_unescape/html_unescape.dart';
+
+import 'feed_list.dart';
 
 void main() => runApp(new MyApp());
-
-final String feedUrl = 'https://www.huffingtonpost.ca/feeds/index.xml';
-
-Future<AtomFeed> fetchFeed() async {
-  final response =
-  await http.get(feedUrl);
-
-  if (response.statusCode == 200) {
-    // If the call to the server was successful, parse the JSON
-    return AtomFeed.parse(response.body);
-  } else {
-    // If that call was not successful, throw an error.
-    throw Exception('Failed to load post');
-  }
-}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,85 +13,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Reader Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  void _refresh() {
-    showDialog(
-        context: context,
-        builder: (_) => new AlertDialog(
-          title: new Text("Coming soon!"),
-          content: new Text("This feature is coming soon."),
-        )
-    );
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    var unescape = new HtmlUnescape();
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Flexible(
-              child: FutureBuilder<AtomFeed>(
-                future: fetchFeed(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                      itemCount: snapshot.data.items.length,
-                      itemBuilder: (BuildContext context, int position) {
-                        return ListTile(
-                          title: Text(
-                            unescape.convert(snapshot.data.items[position].title),
-                            style: Theme.of(context).textTheme.headline
-                          ),
-                          subtitle: Text(
-                              unescape.convert(snapshot.data.items[position].summary),
-                              style: Theme.of(context).textTheme.caption
-                          ),
-                          contentPadding: EdgeInsetsDirectional.only(
-                            start: 10.0,
-                            end: 10.0,
-                            top: 15.0,
-                            bottom: 15.0,
-                          ),
-                        );
-                      },
-                    );
-                  } else if (snapshot.hasError) {
-                    return Text("ERROR: ${snapshot.error}");
-                  }
-                  return CircularProgressIndicator();
-                }
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _refresh,
-        tooltip: 'Refresh',
-        child: new Icon(Icons.refresh),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: new FeedList(title: 'Reader Home Page'),
     );
   }
 }
